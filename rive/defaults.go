835fa4f51347047c6c4c40117c98b707b19c91ee
Coder <coder@redriot.io>
2026-05-13 08:41:59 +0000

    fix: go:generate directive includes --types-out and --toc-out flags

--- rive/defaults.go
+++ rive/defaults.go
@@ -1,3 +1,3 @@
 package rive
 
-//go:generate go run ../cmd/gen-defaults/main.go --contract ../format_contract_proven.json --out gen_defaults.go --package rive
+//go:generate go run ../cmd/gen-defaults/main.go --contract ../format_contract_proven.json --out gen_defaults.go --package rive --types-out gen_defaults_types.txt --toc-out gen_toc_rules.go
